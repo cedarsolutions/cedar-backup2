272b138c8cf31e70d664003f952ecacaf04f9c94
pronovic <pronovic@ieee.org>
2005-07-08 14:34:37 +0000

    Update docs to modify use of "secure"

--- CedarBackup2/__init__.py
+++ CedarBackup2/__init__.py
@@ -21,15 +21,19 @@
 ########################################################################
 
 """
-Implements secure remote and local backups to CD-R or CD-RW.
+Implements local and remote backups to CD-R/CD-RW media.
 
-Cedar Backup is a Python package that supports secure backups of files
-on local and remote hosts to CD-R or CD-RW media.  The package is
-focused around weekly backups to a single disc, with the expectation
-that the disc will be changed or overwritten at the beginning of each
-week.  If your hardware is new enough, the script can write multisession
-discs, allowing you to add to a disc in a daily fashion.  Directories
-are backed up using tar and may be compressed using gzip or bzip2.
+Cedar Backup is a Python package that supports backups of files on local and
+remote hosts to CD-R or CD-RW media over a secure network connection.  Cedar
+Backup also includes extensions that understand how to back up MySQL databases
+and Subversion repositories, and it can be easily extended to support other
+data sources, as well.
+
+The package is focused around weekly backups to a single disc, with the
+expectation that the disc will be changed or overwritten at the beginning of
+each week.  If your hardware is new enough, Cedar Backup can write multisession
+discs, allowing you to add to a disc in a daily fashion.  Directories are
+backed up using tar and may be compressed using gzip or bzip2.
 
 @author: Kenneth J. Pronovici <pronovic@ieee.org>
 """
