469856ba897439361b56cc1f73530ed02b6617ef
pronovic <pronovic@localhost>
2007-07-11 01:27:28 +0000

    Release 2.13.2

--- CedarBackup2/release.py
+++ CedarBackup2/release.py
@@ -34,7 +34,7 @@ Provides location to maintain version information.
 AUTHOR      = "Kenneth J. Pronovici"
 EMAIL       = "pronovic@ieee.org"
 COPYRIGHT   = "2004-2007"
-VERSION     = "2.13.1"
-DATE        = "29 Mar 2007"
+VERSION     = "2.13.2"
+DATE        = "10 Jul 2007"
 URL         = "http://cedar-solutions.com/software/cedar-backup"
 
