bdd4657bb2527487fd30d333f5e7b1ba0d897197
pronovic <pronovic@ieee.org>
2014-10-01 22:00:33 +0000

    Change release to 2.23.1

--- CedarBackup2/release.py
+++ CedarBackup2/release.py
@@ -34,7 +34,7 @@ Provides location to maintain version information.
 AUTHOR      = "Kenneth J. Pronovici"
 EMAIL       = "pronovic@ieee.org"
 COPYRIGHT   = "2004-2011,2013,2014"
-VERSION     = "2.23.0"
+VERSION     = "2.23.1"
 DATE        = "01 Oct 2014"
 URL         = "http://cedar-backup.sourceforge.net/"
 
