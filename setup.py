9584ff8d4df2dc720d02a79eccd0b0cea46acd55
pronovic <pronovic@ieee.org>
2014-10-08 02:08:39 +0000

    Fix setup.py to properly install cback-amazons3-sync

--- setup.py
+++ setup.py
@@ -65,6 +65,6 @@ setup (
    platforms        = ('Any',),
    packages         = ['CedarBackup2', 'CedarBackup2.actions', 'CedarBackup2.extend', 
                        'CedarBackup2.tools', 'CedarBackup2.writers', ],
-   scripts          = ['cback', 'util/cback-span', ], 
+   scripts          = ['cback', 'util/cback-span', 'util/cback-amazons3-sync', ], 
 )
 
