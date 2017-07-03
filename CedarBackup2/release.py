484b89b87646f0c64ea73c035222a3453acf8d03
Kenneth J. Pronovici <pronovic@ieee.org>
2017-07-03 23:42:12 +0000

    Release v2.26.6

--- CedarBackup2/release.py
+++ CedarBackup2/release.py
@@ -32,8 +32,8 @@ Provides location to maintain version information.
 
 AUTHOR      = "Kenneth J. Pronovici"
 EMAIL       = "pronovic@ieee.org"
-COPYRIGHT   = "2004-2011,2013-2016"
-VERSION     = "2.26.5"
-DATE        = "02 Jan 2016"
+COPYRIGHT   = "2004-2011,2013-2017"
+VERSION     = "2.26.6"
+DATE        = "03 Jul 2017"
 URL         = "https://bitbucket.org/cedarsolutions/cedar-backup2"
 
