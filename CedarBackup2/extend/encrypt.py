04a6f2a3cab7432e406546bc538fb09d9061ada3
Kenneth J. Pronovici <pronovic@ieee.org>
2017-07-03 22:25:03 +0000

    Deal with warnings from pylint 1.7.2

--- CedarBackup2/extend/encrypt.py
+++ CedarBackup2/extend/encrypt.py
@@ -381,6 +381,7 @@ class LocalConfig(object):
 # executeAction() function
 ###########################
 
+# pylint: disable=W0613
 def executeAction(configPath, options, config):
    """
    Executes the encrypt backup action.
--- CedarBackup2/extend/split.py
+++ CedarBackup2/extend/split.py
@@ -389,6 +389,7 @@ class LocalConfig(object):
 # executeAction() function
 ###########################
 
+# pylint: disable=W0613
 def executeAction(configPath, options, config):
    """
    Executes the split backup action.
